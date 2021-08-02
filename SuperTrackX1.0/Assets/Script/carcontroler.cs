31119f5643d11972b710eeb86019f34362f41ae9
otaro36 <68829944+otaro36@users.noreply.github.com>
2021-08-02 00:24:49 -0500

    script carro
    el scrip del carro y primera pista

--- SuperTrackX1.0/Assets/Script/carcontroler.cs
+++ SuperTrackX1.0/Assets/Script/carcontroler.cs
@@ -2,37 +2,62 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
 
 public class carcontroler : MonoBehaviour
 {
-    public WheelCollider llantadelanteraIZQWC;
-    public WheelCollider llantadelanteraDERWC;
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+    public Transform target;
+    public float motor;
+    public float steering;
 
-    public float torque = 200;
-    public float acelerracion;
-    public float fuerzatorque;
-
-    public float pedal;
-
-    public void ControlPedal()
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
     {
-        pedal = acelerracion;
-        pedal += 0.1f; 
-    }
-    public void Arranque()
-    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
 
-    }
+        Transform visualWheel = collider.transform.GetChild(0);
 
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
 
-
-    private void Update()
-    {
-        llantadelanteraDERWC.motorTorque = torque * Input.GetAxis("Vertical");
-        llantadelanteraIZQWC.motorTorque = torque * Input.GetAxis("Vertical");
-
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
     }
 
+    public void FixedUpdate()
+    {
+        motor = maxMotorTorque * Input.GetAxis("Vertical");
+        steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
 
 }
